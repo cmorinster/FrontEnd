e24a27b16a08072c5a132577572cb2ab9eb8aa69
FrontEndDanni <dannikabdull@gmail.com>
2023-10-25 01:14:11 -0700

    added pollcreate page

--- src/pages/PollCreate.js
+++ src/pages/PollCreate.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../styling/PollCreate.css';
 
 export default function PollCreate() {
@@ -9,14 +9,9 @@ export default function PollCreate() {
       <div className="pollcreate-modal">
         <div className="modal-content">
           <form>
-            <label htmlFor="name">Your Name*</label><br />
+            <h2>Poll Details</h2>
+            <label htmlFor="name">Poll Name*</label><br />
             <input type="text" id="name" name="name" placeholder="Enter your name here" /><br />
-
-            <label htmlFor="email">Your Email*</label><br />
-            <input type="text" id="email" name="email" placeholder="Enter your email here" /><br />
-
-            <label htmlFor="pollName">Poll Name*</label><br />
-            <input type="text" id="pollName" name="pollName" placeholder="Enter your poll name here" /><br />
 
             <label htmlFor="timeZone">Time Zone (Optional)</label><br />
             <select id="timeZone" name="timeZone">
@@ -31,8 +26,33 @@ export default function PollCreate() {
           </form>
         </div>
       </div>
-      <button type="submit">Next</button>
+
+      <div className="pollpage-title">Poll Details</div>
+
+      <div className="pollcreate-modal">
+        <div className="modal-content">
+          <form>
+            <h2>Poll Details</h2>
+            <label htmlFor="name">Poll Name*</label><br />
+            <input type="text" id="name" name="name" placeholder="Enter your name here" /><br />
+
+            <label htmlFor="timeZone">Time Zone (Optional)</label><br />
+            <select id="timeZone" name="timeZone">
+              <option value="">- Select -</option>
+              <option value="Red">Red</option>
+              <option value="Blue">Blue</option>
+              <option value="Green">Green</option>
+            </select>
+
+            <label htmlFor="description">Description (Optional)</label><br />
+            <textarea id="description" name="description" rows="4" cols="50" placeholder="Enter a more detailed description here..."></textarea><br />
+          </form>
+        </div>
       </div>
+      <div className="button-container">
+        <button type="submit" className="back-button">Back</button>
+        <button type="submit" className="next-button">Create Poll</button>
+      </div>
+    </div>
   );
 }
-
